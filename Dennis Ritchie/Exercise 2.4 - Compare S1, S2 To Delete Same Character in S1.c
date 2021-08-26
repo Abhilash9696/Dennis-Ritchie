f12c7d92c118e61b274bec8bd2d556cdbea82891
Abhilash9696 <87365932+Abhilash9696@users.noreply.github.com>
2021-08-26 14:28:38 +0530

    Update Exercise 2.4 - Compare S1, S2 To Delete Same Character in S1.c

--- Dennis Ritchie/Exercise 2.4 - Compare S1, S2 To Delete Same Character in S1.c
+++ Dennis Ritchie/Exercise 2.4 - Compare S1, S2 To Delete Same Character in S1.c
@@ -1,53 +1,60 @@
 #include<stdio.h>
 #define MAXLINE 1000
 
-int mgetline(char line[],int maxline);
-void squeeze(char s1[],char s2[]);
-
-int main(void)
-{
-    char s1[MAXLINE],s2[MAXLINE];
-    
-    putchar('s');
-    putchar('1');
-    mgetline(s1,MAXLINE);
-
-    putchar('s');
-    putchar('2');
-    mgetline(s2,MAXLINE);
-
-    squeeze(s1,s2);
-
-    printf("%s",s1);
-
-    return 0;
-}
-
-int mgetline(char s[],int lim)
+// Function to read a string or in this case we can even read a line and consider blank spaces, numbers for comparision.
+int getline(char s[],int lim)
 {
     int i,c;
     
-    for(i=0;i<lim-1 && (c=getchar())!=EOF && c != '\n';++i)
+    for(i=0;i<lim-1 && (c=getchar())!=EOF && c != '\n';i++)
+    {
         s[i] = c;
+    }
 
     if(c == '\n')
+    {
         s[i++] = c;
+    }
     
     s[i] = '\0';
 }
 
-void squeeze(char s1[],char s2[])
+//The below function compares s1 and s2 and if a character in s1 exists in s2, deletes that character from s1 and reorders the characters.
+void Comp_and_Del(char s1[],char s2[])
 {
     int i,j,k;
     k=0;
 
     for(i=0;s1[i]!='\0';++i)
     {
-        for(j=0; (s1[i]!=s2[j]) && s2[j]!='\0' ;++j)
+        for(j=0; (s1[i]!=s2[j]) && s2[j]!='\0' ;j++)
+        {
             ;
+        }
         if(s2[j]=='\0')
+        {
             s1[k++] = s1[i];
+        }
     }
     
     s1[k]='\0';
 }
+
+
+void main(void)
+{
+    char s1[MAXLINE],s2[MAXLINE];
+    
+    // Reading two strings.
+    printf("Enter the first string");
+    getline(s1,MAXLINE);
+    printf("Enter the second string");
+    getline(s2,MAXLINE);
+    Comp_and_Del(s1,s2);
+
+    printf("%s",s1);//Prints the modified string s1 which has no characters similar to s2.
+
+}
+
+
+
