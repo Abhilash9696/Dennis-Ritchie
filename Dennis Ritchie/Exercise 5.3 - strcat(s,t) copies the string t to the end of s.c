45d12c01af6c8ec66a6e6170c46d9fc7b19ab6a0
Abhilash9696 <87365932+Abhilash9696@users.noreply.github.com>
2021-08-26 17:36:24 +0530

    Update Exercise 5.3 - strcat(s,t) copies the string t to the end of s.c

--- Dennis Ritchie/Exercise 5.3 - strcat(s,t) copies the string t to the end of s.c
+++ Dennis Ritchie/Exercise 5.3 - strcat(s,t) copies the string t to the end of s.c
@@ -1,30 +1,7 @@
 #include<stdio.h>
 #define MAXLINE 1000
 
-int mgetline(char line[],int maxline);
-void mystrcat(char *,char *);
-
-int main(void)
-{
-    int len;
-    char s[MAXLINE],t[MAXLINE];
-
-    putchar('s');
-    putchar(':');
-    mgetline(s,MAXLINE);
-    
-    putchar('t');
-    putchar(':');
-    mgetline(t,MAXLINE);
-    
-    mystrcat(s,t);
-
-    printf("%s",s);
-
-    return 0;
-}
-
-int mgetline(char s[],int lim)
+int getline(char s[],int lim)
 {
     int c,i;
     
@@ -41,15 +18,31 @@ int mgetline(char s[],int lim)
 
     return i;
 }
-
-void mystrcat(char *s,char *t)
+void string_cat(char *s,char *t)
 {
     while(*s!='\0')
         s++;
-    s--;             /* goes back to \0 char */
+    s--;             
     while((*s=*t)!='\0')
     {   
         s++;
         t++;
     }
 }
+
+int main()
+{
+    int len;
+    char s[MAXLINE],t[MAXLINE];
+
+    printf("Enter the first strin");
+    getline(s,MAXLINE);
+    printf("Enter the second string");
+    getline(t,MAXLINE);
+    string_cat(s,t);
+    printf("%s",s);
+    return 0;
+}
+
+
+
