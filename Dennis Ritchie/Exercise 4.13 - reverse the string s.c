917786390d44cf45d92424da4ad8e45c4279e85a
Abhilash9696 <87365932+Abhilash9696@users.noreply.github.com>
2021-08-26 17:06:01 +0530

    Update Exercise 4.13 - reverse the string s.c

--- Dennis Ritchie/Exercise 4.13 - reverse the string s.c
+++ Dennis Ritchie/Exercise 4.13 - reverse the string s.c
@@ -1,16 +1,25 @@
 #include<stdio.h>
 #include<string.h>
-
 #define MAXLINE 100
+int getline(char s[],int lim)
+{
+    int i,c;
 
-int mgetline(char line[],int maxline);
+    for(i=0;i<lim-1 && (c=getchar())!=EOF && c!='\n';i++)
+        s[i] = c;
+
+    if(c=='\n')
+        s[i++]='\n';
+
+    s[i]='\0';
+}
 void reverse(char s[]);
 
-int main(void)
+int main()
 {
     char s[MAXLINE];
 
-    mgetline(s,MAXLINE);
+    getline(s,MAXLINE);
 
     reverse(s);
 
@@ -19,18 +28,7 @@ int main(void)
     return 0;
 }
 
-int mgetline(char s[],int lim)
-{
-    int i,c;
 
-    for(i=0;i<lim-1 && (c=getchar())!=EOF && c!='\n';++i)
-        s[i] = c;
-
-    if(c=='\n')
-        s[i++]='\n';
-
-    s[i]='\0';
-}
 
 void reverse(char s[])
 {
@@ -39,20 +37,3 @@ void reverse(char s[])
     reverser(s,0,strlen(s));
 }
 
-/* reverser: reverse string s in place; recursive */
-
-void reverser(char s[],int i,int len)
-{
-    int c,j;
-
-    j = len - (i + 1);
-    
-    if( i < j )
-    {
-        c = s[i];
-        s[i] = s[j];
-        s[j] = c;
-
-        reverser(s,++i,len);
-    }
-}
