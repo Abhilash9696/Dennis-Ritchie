49dcd340f992d70d7c0a2df5ac587410a84b4669
Abhilash9696 <87365932+Abhilash9696@users.noreply.github.com>
2021-08-26 14:45:39 +0530

    Update Exercise 3.3 - expand short hand notation in s1 into string s2.c

--- Dennis Ritchie/Exercise 3.3 - expand short hand notation in s1 into string s2.c
+++ Dennis Ritchie/Exercise 3.3 - expand short hand notation in s1 into string s2.c
@@ -1,42 +1,31 @@
 #include<stdio.h>
 #define MAXLINE 100
-
-int mgetline(char s[],int maxlimit);
-void expand(char s1[],char s2[]);
-
-int main(void)
-{
-    char s1[MAXLINE],s2[MAXLINE];
-
-    mgetline(s1,MAXLINE);
-
-    expand(s1,s2);
-
-    printf("%s",s2);
-
-    return 0;
-}
-
-int mgetline(char s[],int lim)
+//The below function reads a string.
+int getline(char s[],int limit)
 {
     int i,c;
 
     for(i=0;i<lim-1 && (c=getchar())!=EOF && c!='\n';++i)
+    {
         s[i]=c;
+    }
 
     if(c=='\n')
+    {
         s[i++]=c;
+    }
 
     s[i]='\0';
 }
-
+// Function checks if there is a hyphen and if the characters on the left and right of the hyphen are in ascending order.
+//Then it expands that short hand notation and stores it in string s2. eg:a-d is in s1. Then abcd is stored in s2.
 void expand(char s1[],char s2[])
 {
     int i,j,c;
-
-    i=j=0;
-
+    i=0;
+    j=0;
     while((c=s1[i++]) != '\0')
+    {
         if(s1[i]=='-' && s1[i+1] >=c)
         {
             i++;
@@ -45,6 +34,18 @@ void expand(char s1[],char s2[])
         }
         else
             s2[j++]=c;
+    }
 
     s2[j]='\0';
 }
+
+int main()
+{
+    char s1[MAXLINE],s2[MAXLINE];
+    //reading the input string(line) s1
+    getline(s1,MAXLINE);
+    expand(s1,s2);
+    printf("%s",s2);
+    return 0;
+}
+
