23fdf120d15f781fc9e071204a7b0471c7f91d80
Abhilash9696 <87365932+Abhilash9696@users.noreply.github.com>
2021-08-26 16:59:49 +0530

    Update Exercise 4.10 - Calculator using getline.c

--- Dennis Ritchie/Exercise 4.10 - Calculator using getline.c
+++ Dennis Ritchie/Exercise 4.10 - Calculator using getline.c
@@ -1,22 +1,92 @@
 #include<stdio.h>
-#include<stdlib.h> /* for atof() */
-
-#define MAXOP 100
+#include<stdlib.h>
+#include<ctype.h>
+#define OP_COUNT 100
 #define NUMBER '0'
+#define MAXVAL 100 
+#define MAXLINE 100
 
-int getop(char []);
-void push(double);
-double pop(void);
+int li= 0;  
+int sp = 0;
+double val[MAXVAL];
+char line[MAXLINE];  
+
+
+void push(double f)
+{
+    if(sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("Max stack storage reached. cannot push %g\n",f);
+}
+double pop(void)
+{
+    if(sp > 0)
+        return val[--sp];
+    else
+    {
+        printf("error: stack empty \n");
+        return 0.0;
+    }
+}
+int getline(char s[],int lim)
+{
+    int i,c;
+
+    for(i=0;i<lim-1 && (c=getchar())!=EOF && c!='\n';++i)
+        s[i] =c;
+    
+    if(c=='\n')
+        s[i++] =c;
+
+    s[i]='\0';
+    
+    return i;
+}
+int read_operator(char s[])
+{
+    int c,i;
+
+    if(line[li] == '\0')
+        if(getline(line,MAXLINE) == 0)
+            return EOF;
+        else
+            li =0;
+    
+    while((s[0] = c = line[li++]) == ' ' || c == '\t')
+        ;
+    
+    s[1] = '\0';
+    
+    if(!isdigit(c) && c!= '.')
+        return c;
+    
+    i = 0;
+    
+    if(isdigit(c))
+        while(isdigit(s[++i] = c = line[li++]))
+            ;
+    if( c == '.')
+        while(isdigit(s[++i] = c = line[li++]))
+            ;
+    
+    s[i] = '\0';
+
+    li--;
+
+    return NUMBER;
+}
+
 
 /* reverse polish notation calculator */
 
-int main(void)
+int main()
 {
     int type;
     double op2;
-    char s[MAXOP];
+    char s[OP_COUNT];
     
-    while((type = getop(s)) != EOF)
+    while((type = read_operator(s)) != EOF)
     {
         switch(type)
         {
@@ -49,92 +119,11 @@ int main(void)
     return 0;
 }
 
-#define MAXVAL 100 /* maximum depth of the val stack */
-
-int sp = 0;
-double val[MAXVAL];
-
-/* push : push f onto value stack */
-
-void push(double f)
-{
-    if(sp < MAXVAL)
-        val[sp++] = f;
-    else
-        printf("error: stack full,can't push %g\n",f);
-}
-
-/* pop: pop and return top values from stack */
-
-double pop(void)
-{
-    if(sp > 0)
-        return val[--sp];
-    else
-    {
-        printf("error: stack empty \n");
-        return 0.0;
-    }
-}
 
 
-/* using getline instead of getch and ungetch */
 
-#include<ctype.h>
-#define MAXLINE 100
 
-int mgetline(char line[],int limit);
 
-int li= 0;  /* input line index */
-char line[MAXLINE];  /* one input line */
 
-/* getop: get next operator or numeric operand */
 
-int getop(char s[])
-{
-    int c,i;
 
-    if(line[li] == '\0')
-        if(mgetline(line,MAXLINE) == 0)
-            return EOF;
-        else
-            li =0;
-    
-    while((s[0] = c = line[li++]) == ' ' || c == '\t')
-        ;
-    
-    s[1] = '\0';
-    
-    if(!isdigit(c) && c!= '.')
-        return c;
-    
-    i = 0;
-    
-    if(isdigit(c))
-        while(isdigit(s[++i] = c = line[li++]))
-            ;
-    if( c == '.')
-        while(isdigit(s[++i] = c = line[li++]))
-            ;
-    
-    s[i] = '\0';
-
-    li--;
-
-    return NUMBER;
-}
-
-int mgetline(char s[],int lim)
-{
-    int i,c;
-
-    for(i=0;i<lim-1 && (c=getchar())!=EOF && c!='\n';++i)
-        s[i] =c;
-    
-    if(c=='\n')
-        s[i++] =c;
-
-    s[i]='\0';
-    
-    return i;
-}
